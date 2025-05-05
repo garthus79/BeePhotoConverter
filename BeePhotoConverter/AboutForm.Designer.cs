8326c43185da8dd4ed65dfcce1e3537ae8e269d6
garthus79 <garthbpos@gmail.com>
2025-05-05 22:52:03 +1000

    Fixed a few missing logos / icons

--- BeePhotoConverter/AboutForm.Designer.cs
+++ BeePhotoConverter/AboutForm.Designer.cs
@@ -120,6 +120,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AboutForm";
             this.Text = "About Bee Photo Converter";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
